76cfe532e836aeef1c421e1becb7bb78a02088e8
Дмитрий Сурков <mvstrike17@gmail.com>
2021-05-13 21:42:17 +0300

    Added routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,31 @@ app = Flask(__name__)
 @app.route('/')
 def entry_point():
     return render_template('community.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        pass
+
+
+@app.route('/forgot-password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'GET':
+        return render_template('forgot_password.html')
+    else:
+        pass
+
 
 
 if __name__ == '__main__':
