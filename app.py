b1c39542a7f6b90f33a89b55daee1d21ed3a88fe
kirill <kirdav06@mail.ru>
2021-05-27 20:26:59 +0300

    Version Name

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def entry_point():
     return render_template('community.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/register_person')
+def register_person():
+    return render_template('register_person.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
